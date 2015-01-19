dd4848873b0801beb3dd0c1e6f4bca6d810afa48
Aleks <absolutnik@gmail.com>
2015-01-19 09:32:38 -0500

    Created sync client and added some messaging capabilities.

--- autosyncd.py
+++ autosyncd.py
@@ -1,12 +1,15 @@
 import abc
 import asyncio
 import os
+import pickle
 import subprocess
 
 import pyinotify
 
+import lib.async_zmq
 
 RSYNC = "rsync"
+
 
 class EventHandler(metaclass=abc.ABCMeta):
     '''
@@ -33,14 +36,14 @@ class EventHandler(metaclass=abc.ABCMeta):
 
 
 class LocalSynchronizer(EventHandler):
-    def __init__(self, sync_from, dest, loop):
+    def __init__(self, sync_from, sync_to, loop):
         '''
         @param sync_from - sync_from directory
-        @param dest - destination directory
+        @param sync_to - destination directory
         @param loop - asyncio loop
         '''
         self._sync_from = sync_from
-        self._dest = dest
+        self._sync_to = sync_to
         self._loop = loop
 
     @asyncio.coroutine
@@ -55,7 +58,7 @@ class LocalSynchronizer(EventHandler):
                                                 '--links',
                                                 '-av',
                                                 '--files-from=-',
-                                                self._sync_from, self._dest,
+                                                self._sync_from, self._sync_to,
                                                 stdin=asyncio.subprocess.PIPE,
                                                 stdout=asyncio.subprocess.PIPE)
         proc = yield from create
@@ -118,31 +121,46 @@ class AutoSync:
     '''
     Class responsible for monitoring directories.
     '''
-    def __init__(self, loop):
+    def __init__(self, controller, loop=None):
         '''
+        @param controller - communications controller
         @param loop - asyncio loop
         '''
+        self._controller = controller
         self._loop = loop if loop is not None else asyncio.get_event_loop()
         self._wm = pyinotify.WatchManager()
         self._notifier = pyinotify.AsyncioNotifier(self._wm, self._loop)
 
-    def add_local_syncher(self, sync_from, dest, events):
+    def add_local_syncher(self, sync_from, sync_to, events):
         '''
         Adds a monitor to watch sync_from. Destination gets updated when sync_from
         changes. sync_from and dest must be directories. Both sync_from and
         destination must be located on the same host.
 
         @param sync_from - sync_from directory
-        @param dest - destination directory
+        @param sync_to - destination directory
+        @param events - set of events which trigger this syncher
         '''
-        if not (os.path.isdir(sync_from) & os.path.isdir(dest)):
+        print("Creating local syncher.")
+        if not (os.path.isdir(sync_from) & os.path.isdir(sync_to)):
             raise ValueError("Sync from, and sync to parameters must be folders.")
 
         self._wm.add_watch(sync_from,
                            events,
-                           proc_fun=LocalSynchronizer(sync_from, dest, self._loop))
+                           proc_fun=LocalSynchronizer(sync_from, sync_to, self._loop))
 
-    def add_remote_syncher(self, sync_from, remote_host, dest, events):
+    def add_remote_syncher(self, sync_from, remote_host, sync_to, events):
+        '''
+        Adds a monitor to watch sync_from. sync_to gets updated when sync_from
+        changes. sync_from and dest must be directories. sync_from must be
+        local, and sync_to must be located on the remote_host.
+
+        @param sync_from - sync_from directory
+        @param remote_host - server on which sync_to is located
+        @param sync_to - destination directory
+        @param events - set of events which trigger this syncher
+        '''
+        print("Creating remote syncher.")
         if not os.path.isdir(sync_from):
             raise ValueError("Sync from must be a directory.")
 
@@ -150,22 +168,71 @@ class AutoSync:
                            events,
                            proc_fun=RemoteSynchronizer(sync_from,
                                                        remote_host,
-                                                       dest,
+                                                       sync_to,
                                                        self._loop))
+
+class AutoSyncController:
+    '''
+    This class wraps the AutoSync object and facilitates communication between
+    AutoSync and the rest of the system via ZMQ.
+    '''
+    def __init__(self, loop=None):
+        '''
+        @param loop - asyncio loop
+        '''
+        self._loop = loop if loop is not None else asyncio.get_event_loop()
+        self._auto_sync = AutoSync(self, self._loop)
+        self._sync_rep = lib.async_zmq.SocketFactory.rep_socket(topic="/tmp/add_syncher",
+                                                                on_recv=self._handle_new_synch_path,
+                                                                loop=self._loop)
+
+    def _handle_new_synch_path(self, msgs):
+        '''
+        Handles messages for creating new sync paths.
+
+        @param msgs - msgs containing information about sync directories, and
+                      file events mask(controls what file system events would
+                      trigger the sync)
+        '''
+        try:
+            for msg in msgs:
+                msg_dict = pickle.loads(msg)
+                print("Got new path message:", msg_dict)
+                sync_from = msg_dict['sync_from']
+                sync_to = msg_dict['sync_to']
+                events = msg_dict['events']
+
+                if 'remote_host' in msg_dict:
+                    remote_host = msg_dict['remote_host']
+                    self._auto_sync.add_remote_syncher(sync_from,
+                                            remote_host,
+                                            sync_to,
+                                            events)
+                else:
+                    self._auto_sync.add_local_syncher(sync_from, sync_to, events)
+
+            response = {"success" : True, "error" : None}
+            self._sync_rep.send(pickle.dumps(response))
+
+        except Exception as e:
+            print(e)
+            error_msg = {"success" : False, "error" : e}
+            self._sync_rep.send(pickle.dumps(error_msg))
 
 
 def main():
     loop = asyncio.get_event_loop()
-    auto_sync = AutoSync(loop)
+    auto_sync_controller = AutoSyncController(loop)
 #    auto_sync.add_local_syncher("/tmp/source",
 #                                "/tmp/dest",
 #                                pyinotify.IN_CREATE | pyinotify.IN_MODIFY)
-    auto_sync.add_remote_syncher("/tmp/source",
-                                 "192.168.1.9",
-                                 "/home/sasha/test",
-                                 pyinotify.IN_CREATE | pyinotify.IN_MODIFY)
+#    auto_sync.add_remote_syncher("/tmp/source",
+#                                 "192.168.1.9",
+#                                 "/home/sasha/test",
+#                                 pyinotify.IN_CREATE | pyinotify.IN_MODIFY)
 
     try:
+        print("Awaiting new messages.")
         loop.run_forever()
     except (SystemExit, KeyboardInterrupt):
         print("Exiting...")
---
+++ sync_client.py
@@ -0,0 +1,166 @@
+'''
+This module provides functionality required to communicate with the AutoSync daemon.
+'''
+import argparse
+import asyncio
+import functools
+import pickle
+import sys
+
+import pyinotify
+
+import lib.async_zmq
+
+
+EVENTS_MAP = {
+    'c' : pyinotify.IN_CREATE,
+    'm' : pyinotify.IN_MODIFY,
+    'f' : pyinotify.IN_MOVED_FROM,
+    't' : pyinotify.IN_MOVED_TO,
+    'd' : pyinotify.IN_DELETE
+}
+
+EVENTS_DESC = """File events that would trigger the synchronization:
+'c' - file created
+'m' - file modified
+'f' - file moved from a watched directory
+'t' - file moved to a watched directory
+'d' - file deleted
+"""
+
+
+class SyncClient:
+    '''
+    Synchronization client for communicating with the auto sync daemon,
+    creating, deleting, or modifying directory synchers.
+    '''
+    def __init__(self, loop):
+        '''
+        @param loop - asyncio loop
+        '''
+        self._loop = loop if loop is not None else asyncio.get_event_loop()
+        self._new_path_req = lib.async_zmq.SocketFactory.req_socket("/tmp/add_syncher",
+                                                                    on_send=self._on_new_path_send,
+                                                                    on_recv=self._on_new_path_recv,
+                                                                    loop=self._loop)
+        self._awaiting_ack = False
+        self._last_msgs = None
+
+    def create_new_path(self, sync_from, sync_to, events, server=None):
+        '''
+        Sends a message to auto sync daemon to watch a new path.
+
+        @param sync_from - sync_from directory
+        @param sync_to - destination directory
+        @param events - string represting events which will trigger the new
+                        path to be updated
+        @param server - server on which sync_to is located
+        '''
+        if self._awaiting_ack:
+            print("Previous path has not been processed yet, can't set new one yet.")
+            return
+
+        if events.strip() == "":
+            print("You must specify some events to trigger synchronization.")
+            self._loop.stop()
+            return
+
+        event_mask = functools.reduce(lambda x, y: EVENTS_MAP[x] | EVENTS_MAP[y], events)
+        new_path_msg = {
+            "sync_from" : sync_from,
+            "sync_to" : sync_to,
+            "events" : event_mask
+        }
+
+        if server is not None:
+            new_path_msg['remote_host'] = server
+
+        self._new_path_req.send(pickle.dumps(new_path_msg))
+
+    def _on_new_path_send(self, msgs):
+        '''
+        Callback triggered when messages are sent on _new_path_req.
+
+        @param msgs - list of sent messages
+        '''
+        self._last_msgs = msgs
+        self._awaiting_ack = True
+
+
+    def _on_new_path_recv(self, msgs):
+        '''
+        Callback triggered when messages are received on _new_path_req.
+
+        @param msgs - list of received messages
+        '''
+        msg = pickle.loads(msgs[-1])
+        if not msg['success']:
+            print(msg['error'])
+
+        self._last_msgs = None
+        self._awaiting_ack = False
+        self._loop.stop()
+
+
+def timeout(loop):
+    '''
+    This operation gets invoked after a certain amount of time passes and
+    AutoSync daemon still has not responded.
+    '''
+    print("Operation timed out. Can't communicate with the AutoSync daemon.")
+    loop.stop()
+
+def main():
+    parser = argparse.ArgumentParser(description="Client program to communicate "
+                                                 "with the AutoSync daemon.",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    new_path_group = parser.add_argument_group("New Path",
+                                               description="A set of arguments "
+                                               "to create a new sync path.")
+
+    new_path_group.add_argument("-f",
+                                "--from_dir",
+                                help="Directory from which to synchronize files.")
+
+    new_path_group.add_argument("-t",
+                                "--to_dir",
+                                help="Directory to be updated when from_dir is changed.")
+
+    new_path_group.add_argument("-s",
+                                "--server",
+                                help="Server on which to_dir is located.")
+
+    new_path_group.add_argument("-e",
+                                "--events",
+                                help=EVENTS_DESC,
+                                default="cm")
+
+    args = parser.parse_args()
+
+    try:
+        loop = asyncio.get_event_loop()
+        sync_client = SyncClient(loop=loop)
+
+        if args.from_dir and args.to_dir and args.events:
+            print("Creating new sync path: %s => %s at %s" % (args.from_dir,
+                                                              args.to_dir,
+                                                              args.events))
+            loop.call_soon(sync_client.create_new_path,
+                           args.from_dir,
+                           args.to_dir,
+                           args.events,
+                           args.server)
+
+            loop.call_later(2, timeout, loop)
+        else:
+            print("You must supply new path arguments.")
+            sys.exit(0)
+
+        loop.run_forever()
+        print("Here")
+    except (KeyboardInterrupt, SystemExit):
+        print("Exiting...")
+
+
+if __name__ == "__main__":
+    main()
